3dd3710b6af852912e9f33588cc991b1a6afa105
bharatroy4D <bharatroy1005@gmail.com>
2026-03-23 23:34:55 +0600

    project full folder stracture setup

---
+++ src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/home',
+                element: <Home />
+            }
+        ]
+    }
+])
