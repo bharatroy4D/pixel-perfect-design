026ce493db1cfd00eeb19e800b45c9b37b65db42
bharatroy4D <bharatroy1005@gmail.com>
2026-03-31 15:48:48 +0600

    Sevices of full desing complite

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -15,9 +15,9 @@ const Services = () => {
                 <p className='text-base'>Our <span className='text-[#FFA500]'>Story</span></p>
             </div>
             <h1 className='text-center text-5xl py-3'>Scale Smarter, <span className='primary'>Sell Faster</span></h1>
-            <p className='text-center text-lg'>We handle everything Amazon so you can focus on growing.</p>
+            <p className='text-center text-base'>We handle everything Amazon so you can focus on growing.</p>
             {/* services card */}
-            <div className='px-25'>
+            <div className='grid grid-cols-2 gap-10 space-y-12 pt-14 px-25'>
                 {/* card-1 */}
                 <div className=''>
                     <div className='flex justify-between'>
@@ -27,11 +27,59 @@ const Services = () => {
                     <img src={line} alt="" className=''/>
                     <div className='flex items-center gap-3'>
                         <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
-                        <p className='text-base'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
                     </div>
                     <div className='flex items-center gap-3'>
                         <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
-                        <p className='text-base'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                </div>
+                {/* card-2 */}
+                <div className=''>
+                    <div className='flex justify-between'>
+                        <h1 className='text-lg primary'>Amazon Growth & Advertising</h1>
+                        <img src={ranking} alt="" className='w-16' />
+                    </div>
+                    <img src={line} alt="" className=''/>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                </div>
+                {/* card-3 */}
+                <div className=''>
+                    <div className='flex justify-between'>
+                        <h1 className='text-lg primary'>Amazon Growth & Advertising</h1>
+                        <img src={account} alt="" className='w-16' />
+                    </div>
+                    <img src={line} alt="" className=''/>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                </div>
+                {/* card-4 */}
+                <div className=''>
+                    <div className='flex justify-between'>
+                        <h1 className='text-lg primary'>Amazon Growth & Advertising</h1>
+                        <img src={global} alt="" className='w-16' />
+                    </div>
+                    <img src={line} alt="" className=''/>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className='text-base secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        <img src={baseCicle} alt="" className='border border-amber-500 rounded-full p-0.5 w-4 h-4' />
+                        <p className=' secondary'>Bid & Budget Optimization – Higher ROI, lower ACOS.</p>
                     </div>
                 </div>
             </div>
